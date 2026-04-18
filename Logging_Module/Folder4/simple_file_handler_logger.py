d1d2768874d86aaa6c4c2f576e4038e5eddb86a0
ashish-543 <ashpnd543@gmail.com>
2026-04-18 19:17:11 +0545

    Create simple_file_handler_logger.py

---
+++ Logging_Module/Folder4/simple_file_handler_logger.py
@@ -0,0 +1,26 @@
+import logging
+import logging.config
+
+import yaml
+
+logger = logging.getLogger("app")
+
+def configure_logger():
+    with open('logger_config.yaml', 'r', encoding = 'utf-8') as f:
+        config = yaml.safe_load(f)
+
+    logging.config.dictConfig(config)
+
+def main():
+    logger.error("This is error message: %s %d", 'argument', 100) # Using f{} is not recommended so use %
+    logger.warning("This is warning message: %d %s", 101, 'parameter')
+    logger.debug("This is debug message")
+
+
+if __name__ == "__main__":
+    configure_logger()
+    main()
+
+# Output Inside File
+# 2026-04-18 19:14:31,840 | app | ERROR | main | This is error message: argument 100
+# 2026-04-18 19:14:31,840 | app | WARNING | main | This is warning message: 101 parameter
